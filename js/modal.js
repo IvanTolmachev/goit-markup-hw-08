64f424777319f4140acfcb6388551dee0285065d
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2022-10-19 20:59:09 +0300

    edits

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,3 @@
-const { height: pageHeaderHeight } = document
-  .querySelector('.page-header')
-  .getBoundingClientRect();
-
-document.body.style.paddingTop = `${pageHeaderHeight}px`;
-
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
